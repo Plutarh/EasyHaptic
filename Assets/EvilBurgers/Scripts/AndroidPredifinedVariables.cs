ad7ffbcaae40561a28661440f1738ed3d47ba46c
George <55986112+Plutarh@users.noreply.github.com>
2022-02-04 16:02:52 +0300

    android predifined values changed

--- Assets/EvilBurgers/Scripts/AndroidPredifinedVariables.cs
+++ Assets/EvilBurgers/Scripts/AndroidPredifinedVariables.cs
@@ -3,11 +3,11 @@ namespace EasyHaptic_EvilBurgers
     public sealed class AndroidPredifinedVariables
     {
         // Duration on milliseconds
-        public static long lightImpactDuration = 20;
+        public static long lightImpactDuration = 40;
         public static long mediumImpactDuration = 75;
         public static long heavyImpactDuration = 120;
 
-        // Amplitude of vibration. Remeber if you want add custom variables amplitude must be 'INT' between 0-255 where 0 is 'NO VIBRATION'
+        // Amplitude of vibration. Remember if you want add custom variables amplitude must be 'INT' between 0-255 where 0 is 'NO VIBRATION'
         public static int lightImpactAmplitude = 40;
         public static int mediumImpactAmplitude = 120;
         public static int heavyImpactAmplitude = 255;
@@ -15,20 +15,22 @@ namespace EasyHaptic_EvilBurgers
         // Patterns for wave vibration. You can easly play with values. For better effect need add more than 2 values in array
         public static long[] successPatternDuration = {
                                 30,
-                                70,
+                                140,
                                 30,
-                                70
+                                140
                                 };
 
         public static int[] successPatternAmplitude = {
                                 20,
-                                50,
+                                75,
                                 20,
-                                50
+                                75
                                 };
         
         public static long[] warningPatternDuration = { 
                                 50, 
+                                100,
+                                50,
                                 100,
                                 50,
                                 100
@@ -36,23 +38,25 @@ namespace EasyHaptic_EvilBurgers
 
         public static int[] warningPatternAmplitude = { 
                                 60, 
-                                120,
+                                200,
                                 60,
-                                120
+                                200,
+                                60,
+                                200
                                 };
 
         public static long[] failurePatternDuration = {
-                                400,
                                 50,
-                                400,
-                                50
+                                500,
+                                50,
+                                500
                                 };
 
         public static int[] failurePatternAmplitude = {
-                                150,
                                 50,
                                 150,
-                                50 
+                                50,
+                                150 
                                 };
     }
 }
